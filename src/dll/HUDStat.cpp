90780380d749bdd79d85b4a8846ab88d53a10e27
Blupi <mycomeback31@hotmail.com>
2015-10-13 12:49:30 +0200

    2 decimal value to float stats

--- src/dll/HUDStat.cpp
+++ src/dll/HUDStat.cpp
@@ -93,7 +93,7 @@ std::string HUDStat::NumToStr(float number, bool percentage)
     else
     {
         ss.setf(std::ios::fixed, std::ios::floatfield);
-        ss.precision(1);
+        ss.precision(2);
         ss << number;
     }
 
